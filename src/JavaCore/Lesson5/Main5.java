f3cefb9d3aa7046300caf52fe477b72714b94c5e
silantyevmn <5992033@mail.ru>
2017-10-19 01:25:07 +0300

    Усовершенствовал метод МУльтиПотоки, добавил реализацию в несколько потоков.

--- src/JavaCore/Lesson5/Main5.java
+++ src/JavaCore/Lesson5/Main5.java
@@ -1,8 +1,5 @@
 package JavaCore.Lesson5;
 
-import JavaCore.Lesson1.Main;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -42,26 +39,25 @@ import java.util.Arrays;
  ** Создать универсальный метод, который делит просчет массива на столько потоков, сколько ему было передано.
  */
 public class Main5 {
-    private static final int SIZE = 100;
-    private static final int SIZE_SMALL = SIZE / 2;
-    public static volatile float[] arr = new float[SIZE];
-    public static float[] singleTreadArr = new float[SIZE];
-    public static float[] multiTreadArr = new float[SIZE];
+    private static final int SIZE = 10000000; //размер массива
+    private static final int COUNT_THREAD = 4; // кол-во потоков
+    public static int VALUE_DEFAULT_ARR=1; // заполнить массив 1
+    public static float[] arr = new float[SIZE]; // исходный массив
+    public static float[] singleTreadArr = new float[SIZE]; //массив после прохода в один поток
+    public static float[] multiTreadArr = new float[SIZE]; // массив после прохода COUNT_THREAD потоков
     public static long singleTreadTime;
     public static long multiTreadTime;
 
     public static void main(String[] args) {
-        init(1);
-        System.out.println("метод1 итого: "+singleTread());
-        System.out.println("метод2 итого:"+multiTread());
-
+        init(VALUE_DEFAULT_ARR);
+        System.out.println("итого один поток: "+singleTread());
+        System.out.println("итого "+COUNT_THREAD+" потока:"+multiTread());
+        System.out.printf("Производительность %.2f%%",(1-(double)multiTreadTime/(double)singleTreadTime)*100);
         //проверка массивов
-        System.out.println("\nКонтрольная проверка"+ Arrays.equals(singleTreadArr,multiTreadArr));
-        System.out.println("Массив1-"+result(singleTreadArr));
-        System.out.println("Массив2-"+result(multiTreadArr));
+        System.out.println("\nКонтрольная проверка-"+ Arrays.equals(singleTreadArr,multiTreadArr));
+        System.out.println("Массив1 сумма:"+result(singleTreadArr));
+        System.out.println("Массив2 сумма:"+result(multiTreadArr));
     }
-
-
 
     public static long singleTread(){
         long startTime = System.currentTimeMillis();
@@ -74,35 +70,36 @@ public class Main5 {
     }
 
     public static long multiTread() {
+        int tempSizeArr=SIZE/COUNT_THREAD;
         long startTime = System.currentTimeMillis();
-        float[] a1 = new float[SIZE_SMALL];
-        float[] a2 = new float[SIZE_SMALL];
-
         long a=System.currentTimeMillis();
-        System.arraycopy(arr, 0, a1, 0, SIZE_SMALL);
-        System.arraycopy(arr, SIZE_SMALL, a2, 0, SIZE_SMALL);
-        print(a,"разбивка массивов");
-
-        a=System.currentTimeMillis();
-        MyThread t1=new MyThread("метод2-поток1 ",a1,0);
-        MyThread t2=new MyThread("метод2-поток2 ",a2,SIZE_SMALL);
-        t1.start();
-        t2.start();
-        try {
-            t1.join();
-            print(a,"расчет 1 массив");
-            a=System.currentTimeMillis();
-            t2.join();
-            print(a,"расчет 2 массив");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        float[][] floats=new float[COUNT_THREAD][tempSizeArr];
+        for (int i = 0; i < floats.length; i++) {
+            floats[i]=new float[tempSizeArr];
+            System.arraycopy(arr, i*tempSizeArr, floats[i], 0, tempSizeArr);
         }
+        print(a,"разбивка");
+
+        MyThread[] thread=new MyThread[COUNT_THREAD];
+        a=System.currentTimeMillis();
+        for (int i = 0; i < thread.length; i++) {
+            try {
+            thread[i]=new MyThread("поток-"+i,floats[i],i*tempSizeArr);
+            thread[i].start();
+                thread[i].join();
+                //print(a,"расчет "+i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        print(a,"расчет");
 
         a=System.currentTimeMillis();
-        System.arraycopy(t1.getArr(), 0, multiTreadArr, 0, SIZE_SMALL);
-        System.arraycopy(t2.getArr(), 0, multiTreadArr, SIZE_SMALL, SIZE_SMALL);
-        print(a,"склейка массивов");
-
+        for (int i = 0; i < floats.length; i++) {
+            if(i==0) System.arraycopy(thread[i].getArr(), 0, multiTreadArr, 0, tempSizeArr);
+            else System.arraycopy(thread[i].getArr(), 0, multiTreadArr, tempSizeArr*i, tempSizeArr);
+        }
+        print(a,"склейка");
         multiTreadTime=System.currentTimeMillis()-startTime;
         return multiTreadTime;
     }
@@ -123,6 +120,6 @@ public class Main5 {
 
     public static void print(long a,String text){
         long countTime=System.currentTimeMillis()-a;
-        System.out.printf(text+" "+countTime+" м.сек. ");
+        System.out.printf(text+"-"+countTime+",");
     }
 }
