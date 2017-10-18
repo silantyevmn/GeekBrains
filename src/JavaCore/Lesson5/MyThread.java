b182be57cf6a33566a98556c94c924ddc48f7043
silantyevmn <5992033@mail.ru>
2017-10-18 23:48:49 +0300

    класс потоков дз 5 урока

--- src/JavaCore/Lesson5/MyThread.java
+++ src/JavaCore/Lesson5/MyThread.java
@@ -4,5 +4,31 @@ package JavaCore.Lesson5;
  * JavaCore.Lesson5
  * Created by Михаил Силантьев on 18.10.2017.
  */
-public class MyThread {
+public class MyThread extends Thread{
+    private volatile float[] arr;
+    private volatile int countArr=0;
+
+    @Override
+    public void run(){
+        //System.out.println(Thread.currentThread().getName());
+        metod(countArr);
+    }
+
+    MyThread(String name,float[] arr,int countArr){
+        super(name);
+        this.arr=arr;
+        this.countArr=countArr;
+    }
+
+    public synchronized float[] getArr() {
+        return arr;
+    }
+
+    public synchronized long metod(int h){
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + h) / 5) * Math.cos(0.2f + (i + h) / 5) * Math.cos(0.4f + (i + h) / 2));
+        }
+        return System.currentTimeMillis()-start;
+    }
 }
