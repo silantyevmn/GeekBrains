f1b2aea0cbcaead6ad79e18ed91e3be13236c1a2
silantyevmn <5992033@mail.ru>
2017-10-19 23:49:00 +0300

    убрал из метода и переменных монитор(sinchronize)

--- src/JavaCore/Lesson5/MyThread.java
+++ src/JavaCore/Lesson5/MyThread.java
@@ -5,8 +5,8 @@ package JavaCore.Lesson5;
  * Created by Михаил Силантьев on 18.10.2017.
  */
 public class MyThread extends Thread{
-    private volatile float[] arr;
-    private volatile int countArr=0;
+    private float[] arr;
+    private int countArr=0;
 
     @Override
     public void run(){
@@ -20,11 +20,11 @@ public class MyThread extends Thread{
         this.countArr=countArr;
     }
 
-    public synchronized float[] getArr() {
+    public float[] getArr() {
         return arr;
     }
 
-    public synchronized long metod(int h){
+    public long metod(int h){
         long start = System.currentTimeMillis();
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + h) / 5) * Math.cos(0.2f + (i + h) / 5) * Math.cos(0.4f + (i + h) / 2));
