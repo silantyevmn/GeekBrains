074e13c184ff0c8c9c3fed8bbfee36db90b41286
silantyevmn <5992033@mail.ru>
2017-10-24 21:04:43 +0300

    Интерфейс авторизации

--- src/JavaCore/Lesson7/server/AuthService.java
+++ src/JavaCore/Lesson7/server/AuthService.java
@@ -5,4 +5,7 @@ package JavaCore.Lesson7.server;
  * Created by Михаил Силантьев on 24.10.2017.
  */
 public interface AuthService {
+    void start();
+    void stop();
+    String getNickByLoginPass(String login, String pass);
 }
