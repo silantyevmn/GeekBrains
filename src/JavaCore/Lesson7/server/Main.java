f9013da2ff1ef20a39d13dc26e22d9d369c178d8
silantyevmn <5992033@mail.ru>
2017-10-24 21:06:24 +0300

    майн запуска сервера

--- src/JavaCore/Lesson7/server/Main.java
+++ src/JavaCore/Lesson7/server/Main.java
@@ -4,7 +4,7 @@ package JavaCore.Lesson7.server;
  * JavaCore.Lesson7.server
  * Created by Михаил Силантьев on 24.10.2017.
  */
-public class StartServer {
+public class Main {
     public static void main(String[] args) {
         new Server();
     }
