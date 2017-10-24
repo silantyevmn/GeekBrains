52071460ac14ef6c739df72a774b18e6e8b76f14
silantyevmn <5992033@mail.ru>
2017-10-24 21:05:22 +0300

    Чат клиент

--- src/JavaCore/Lesson7/client/ChatWindow.java
+++ src/JavaCore/Lesson7/client/ChatWindow.java
@@ -1,38 +1,46 @@
-package Lesson7.client;
+package JavaCore.Lesson7.client;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
- * JavaCore.Lesson7
+ * JavaCore.JavaCore.Lesson7
  * Created by Михаил Силантьев on 24.10.2017.
  */
-public class ChatWindow extends JFrame {
+public class ChatWindow extends JFrame implements ActionListener {
     private final int SIZE_X = 400;
     private final int SIZE_Y = 400;
-    private final int SERVER_PORT = 8189;
+    private final int SERVER_PORT = 8190;
     private final String SERVER_ADRESS = "localhost";
     private JTextField jTextField;
     private JTextArea jTextArea;
-    private Scanner in;
-    private PrintWriter out;
-    private Socket socket;
+    private JTextArea nikName;
+    private JTextField loginText;
+    private JPasswordField passwortText;
+    private JButton jbLogin = new JButton("Логин");
+    private JButton jbSend = new JButton("Отправить");
+    private JPanel jpDown, jpUp,jpRigth;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private Socket socket=null;
+    private boolean isAuthorized;
+
+    public void setAuthorized(boolean authorized) { //скрываем панели для авторизованых и неавторизованых пользователей
+        isAuthorized = authorized;
+        jpUp.setVisible(!isAuthorized);
+        jpDown.setVisible(isAuthorized);
+    }
 
     ChatWindow() {
-        try {
-            socket = new Socket(SERVER_ADRESS, SERVER_PORT);
-            in = new Scanner(socket.getInputStream());
-            out = new PrintWriter(socket.getOutputStream());
-        } catch (IOException e) {
-            System.out.println("Нет соединения с сервером!");
-            //e.printStackTrace();
-        }
         setSize(SIZE_X, SIZE_Y);
         setTitle("ChatWindow");
         setLocationRelativeTo(null);
@@ -44,58 +52,150 @@ public class ChatWindow extends JFrame {
         jTextArea.setLineWrap(true); //перенос текста
         JScrollPane jScrollPane = new JScrollPane(jTextArea);
 
-        JPanel jPanel = new JPanel();
-        jPanel.setLayout(new BorderLayout());
-        jPanel.setPreferredSize(new Dimension(200, 50));
+        jpDown = new JPanel();
+        jpDown.setLayout(new BorderLayout());
+        //jpDown.setPreferredSize(new Dimension(200, 0));
         jTextField = new JTextField();
-        JButton jButton = new JButton("send");
-        jPanel.add(jTextField, BorderLayout.CENTER);
-        jPanel.add(jButton, BorderLayout.EAST);
+        jpDown.add(jTextField, BorderLayout.CENTER);
+        jpDown.add(jbSend, BorderLayout.EAST);
+        jbSend.addActionListener(this);
+        jTextField.addActionListener(this);
 
-        jButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sendMessage();
-            }
-        });
-        jTextField.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sendMessage();
-            }
-        });
+        jpUp = new JPanel();
+        jpUp.setLayout(new GridLayout(1, 3));
+        loginText = new JTextField();
+        loginText.setSize(new Dimension(100,30));
+        passwortText = new JPasswordField();
+        loginText.addActionListener(this);
+        passwortText.addActionListener(this);
+        jbLogin.addActionListener(this);
+        jpUp.add(loginText);
+        jpUp.add(passwortText);
+        jpUp.add(jbLogin);
 
+        jpRigth=new JPanel();
+        jpRigth.setLayout(new BorderLayout());
+        jpRigth.setPreferredSize(new Dimension(150,0));
+        nikName = new JTextArea();
+        nikName.setEditable(false); //запрет на редактирование
+        nikName.setLineWrap(true); //перенос текста
+        JScrollPane scrollPane = new JScrollPane(nikName);
+        jpRigth.add(scrollPane);
+
+        add(jpRigth,BorderLayout.LINE_END);
+        add(jScrollPane, BorderLayout.CENTER);
+        add(jpDown, BorderLayout.SOUTH);
+        add(jpUp, BorderLayout.NORTH);
+        setVisible(true);
+
+        try {
+            socket = new Socket(SERVER_ADRESS, SERVER_PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("Нет соединения с сервером!");
+            e.printStackTrace();
+        }
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true) {
-                    try {
-                        if (in.hasNext()) {
-                            String text = in.nextLine();
-                            jTextArea.append(text);
-                            jTextArea.append("\n");
+                try {
+                    while (true) { // авторизация пользователя
+                        String msg = in.readUTF();
+                        if (msg.startsWith("/authok")) {
+                            setAuthorized(true);
+                            break;
                         }
-                    } catch (Exception e) {
-                        System.out.println("Ошибка! программа будет закрыта!");
-                        System.exit(-1);
-                        //e.printStackTrace();
+                        if (msg.startsWith("/online")) {
+                            updatePersonOnline(msg);
+                            continue;
+                        }
+
+                        jTextArea.append(msg + "\n");
+                        jTextArea.setCaretPosition(jTextArea.getDocument().getLength());
+                    }
+                    while (true) {
+                        String msg = in.readUTF();
+                        if (msg.startsWith("/online")) {
+                            updatePersonOnline(msg);
+                            continue;
+                        }
+                        jTextArea.append(msg + "\n");
+                        jTextArea.setCaretPosition(jTextArea.getDocument().getLength()); //перемещает курсор на самый последний символ чата
+                    }
+                } catch (IOException e) {
+                    //e.printStackTrace();
+                    setAuthorized(false);
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
             }
         }).start();
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                try{
+                    out.writeUTF("/end");
 
-        add(jScrollPane, BorderLayout.CENTER);
-        add(jPanel, BorderLayout.SOUTH);
-        setVisible(true);
-
-
+                    socket.close();
+                }catch(IOException e1){
+                    e1.printStackTrace();
+                    setAuthorized(false);
+                }
+            }
+        });
+        setAuthorized(false);
     }
 
-    private void sendMessage() {
-        String text = jTextField.getText();
-        out.println(text);
-        out.flush();
+    public void auth(){
+        String autText="/auth ";
+        if(loginText.getText().length()<2){
+            autText="/login null";
+        } else if(passwortText.getText().length()<2){
+            autText="/password null";
+        }
+        try{
+            out.writeUTF(autText + loginText.getText() + " " + passwortText.getText());
+            loginText.setText("");
+            passwortText.setText("");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(){
+        String msg = jTextField.getText();
         jTextField.setText("");
+        try{
+            out.writeUTF(msg);
+            out.flush();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
+    public void updatePersonOnline(String msg){
+        //todo Online
+        nikName.setText("Онлайн:\n");
+        String[] elements  = msg.split(" ");
+        for (int i = 1; i < elements.length; i++) {
+            nikName.append(elements[i]+"\n");
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object obj = e.getSource();
+        if (obj.equals(jTextField) || obj.equals(jbSend)) {
+            sendMessage();
+        } else if(obj.equals(loginText) || obj.equals(passwortText) || obj.equals(jbLogin)) auth();
+//        else {
+//            System.out.println(obj.toString() + " не назначен обработчик");
+//        }
+    }
 }
