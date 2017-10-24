d4f6c67a87820f313472cc1b65b826dc9408c7a8
silantyevmn <5992033@mail.ru>
2017-10-24 21:06:12 +0300

    Майн чат клиента

--- src/JavaCore/Lesson7/client/Main.java
+++ src/JavaCore/Lesson7/client/Main.java
@@ -4,7 +4,7 @@ package JavaCore.Lesson7.client;
  * JavaCore.JavaCore.Lesson7
  * Created by Михаил Силантьев on 24.10.2017.
  */
-public class StartClient {
+public class Main {
     public static void main(String[] args) {
         new ChatWindow();
     }
