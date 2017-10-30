7e252f7624de0828bd07af95d1262a2bb032b89d
silantyevmn <5992033@mail.ru>
2017-10-30 16:04:04 +0300

    дз 5 урок

---
+++ src/JavaCore/Lesson5/MyThread.java
@@ -0,0 +1,34 @@
+package JavaCore.Lesson5;
+
+/**
+ * JavaCore.Lesson5
+ * Created by Михаил Силантьев on 18.10.2017.
+ */
+public class MyThread extends Thread{
+    private float[] arr;
+    private int countArr=0;
+
+    @Override
+    public void run(){
+        //System.out.println(Thread.currentThread().getName());
+        metod(countArr);
+    }
+
+    MyThread(String name,float[] arr,int countArr){
+        super(name);
+        this.arr=arr;
+        this.countArr=countArr;
+    }
+
+    public float[] getArr() {
+        return arr;
+    }
+
+    public long metod(int h){
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + h) / 5) * Math.cos(0.2f + (i + h) / 5) * Math.cos(0.4f + (i + h) / 2));
+        }
+        return System.currentTimeMillis()-start;
+    }
+}
