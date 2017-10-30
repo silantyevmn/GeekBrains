6281bb06eb25c1f0a18a829d3b6a17fd876164f9
silantyevmn <5992033@mail.ru>
2017-10-30 16:03:44 +0300

    дз 3 урок

---
+++ src/JavaCore/Lesson3/PhoneBook.java
@@ -0,0 +1,43 @@
+package JavaCore.Lesson3;
+
+import com.sun.javafx.collections.MappingChange;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+/**
+ * 2. Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных номеров.
+ В этот телефонный справочник с помощью метода add() можно добавлять записи. С помощью метода get() искать номер телефона по фамилии.
+ Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев),
+ тогда при запросе такой фамилии должны выводиться все телефоны.
+
+ * JavaCore.Lesson3
+ * Created by Михаил Силантьев on 11.10.2017.
+ */
+public class PhoneBook {
+    private Map<String,HashSet<String>> map;
+
+    public PhoneBook() {
+        map=new HashMap<>();
+    }
+
+    public void add(String name, String phone){
+        HashSet<String> phones;
+        if (map.containsKey(name)) {
+            phones = map.get(name);
+        } else {
+            phones = new HashSet<>();
+        }
+        phones.add(phone);
+        map.put(name,phones);
+    }
+
+    public HashSet<String> get(String name){
+        return map.get(name);
+    }
+
+    public Map<String, HashSet<String>> getMap() {
+        return map;
+    }
+}
