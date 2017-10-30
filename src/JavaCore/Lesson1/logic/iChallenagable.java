4dfa4487f5f37b95bddba6d5b6d05cbcfe8fe013
silantyevmn <5992033@mail.ru>
2017-10-30 16:01:18 +0300

    удалил файлы

--- src/JavaCore/Lesson1/logic/iChallenagable.java
+++
@@ -1,9 +0,0 @@
-package JavaCore.Lesson1.logic;
-
-public interface iChallenagable {
-    void run(int distance);
-    void swim(int distance);
-    void jump(int height);
-    void printResult();
-    void print();
-}
