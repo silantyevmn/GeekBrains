506ff1e2922ff103f96244a8ec062932ff752262
silantyevmn <5992033@mail.ru>
2017-10-30 12:47:32 +0300

    добавил 8 урок

---
+++ src/JavaCore/Lesson8/server/Main.java
@@ -0,0 +1,11 @@
+package JavaCore.Lesson8.server;
+
+/**
+ * JavaCore.Lesson8.server
+ * Created by Михаил Силантьев on 30.10.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Server();
+    }
+}
