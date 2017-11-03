cfabaafaab22464c37bdb8e8a4cdce77a1a76f50
silantyevmn <5992033@mail.ru>
2017-11-03 22:10:16 +0300

    класс яблоко

---
+++ FacultyAndroidDevelopment/3.Java_Core_Professional/src/ru/silantyevmn/java_core_professinal/lesson1/Apple.java
@@ -0,0 +1,12 @@
+package ru.silantyevmn.java_core_professinal.lesson1;
+
+/**
+ * ru.silantyevmn.java_core_professinal.lesson1
+ * Created by Михаил Силантьев on 03.11.2017.
+ */
+public class Apple extends Fruit {
+
+    public Apple() {
+        super(1.f,"Яблоки");
+    }
+}
