48d8892e86be98483ca4ac5b3479f64fa3f52404
silantyevmn <5992033@mail.ru>
2017-11-03 22:09:43 +0300

    класс Апельсин

---
+++ FacultyAndroidDevelopment/3.Java_Core_Professional/src/ru/silantyevmn/java_core_professinal/lesson1/Orange.java
@@ -0,0 +1,12 @@
+package ru.silantyevmn.java_core_professinal.lesson1;
+
+/**
+ * ru.silantyevmn.java_core_professinal.lesson1
+ * Created by Михаил Силантьев on 03.11.2017.
+ */
+public class Orange extends Fruit {
+
+    public Orange() {
+        super(1.5f,"Апельсины");
+    }
+}
