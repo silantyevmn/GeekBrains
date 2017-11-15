6d4c1448db7609cb81e1b6194b7c1fcd66d88781
silantyevmn <5992033@mail.ru>
2017-11-15 17:49:27 +0300

    Поправил метод add. Добавил возможность передать массив Фруктов

--- FacultyAndroidDevelopment/3.Java_Core_Professional/src/ru/silantyevmn/java_core_professinal/lesson1/Box.java
+++ FacultyAndroidDevelopment/3.Java_Core_Professional/src/ru/silantyevmn/java_core_professinal/lesson1/Box.java
@@ -1,12 +1,15 @@
 package ru.silantyevmn.java_core_professinal.lesson1;
 
+import java.awt.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * ru.silantyevmn.java_core_professinal.lesson1
  * Created by Михаил Силантьев on 03.11.2017.
  */
-public class  Box <T extends Fruit>{
+public class Box<T extends Fruit> {
 
     private ArrayList<T> arrayList = new ArrayList<>();
     private String name;
@@ -15,36 +18,35 @@ public class  Box <T extends Fruit>{
         this.name = name;
     }
 
-    void add(T fruit){
-        arrayList.add(fruit);
-    }
-
-    boolean addAll(Box<T> box){
-        try {
-            this.arrayList.addAll(box.arrayList);
-            box.arrayList.clear();
-            return true;
-        } catch (Exception e) {
-            return false;
+    void add(T... fruit) {
+        for (int i = 0; i < fruit.length; i++) {
+            arrayList.add(fruit[i]);
         }
     }
 
-    float getWeight(){
-        float result=0;
+    boolean addAll(Box<T> box) {
+        this.arrayList.addAll(box.arrayList);
+        box.arrayList.clear();
+        return true;
+    }
+
+    float getWeight() {
+        float result = 0;
         for (T fruit : arrayList) {
-            result+=fruit.getWeight();
+            result += fruit.getWeight();
         }
         return result;
     }
-    boolean compare(Box<?> box){
-        return this.getWeight()==box.getWeight();
+
+    boolean compare(Box<?> box) {
+        return Math.abs(this.getWeight()-box.getWeight())<0.000001;
     }
 
     public String getName() {
         return name;
     }
 
-    void print(){
-        System.out.println( this.name+":"+Fruit.name +"-"+getWeight());
+    void print() {
+        System.out.println(this.name + ":" + Fruit.name + "-" + getWeight());
     }
 }
