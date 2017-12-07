2b06fba1b49dba399e144aa39ac155a9460b23fe
silantyevmn <5992033@mail.ru>
2017-12-07 20:38:52 +0300

    анотация test

---
+++ FacultyAndroidDevelopment/3.Java_Core_Professional/src/ru/silantyevmn/java_core_professinal/lesson7/Test.java
@@ -0,0 +1,14 @@
+package ru.silantyevmn.java_core_professinal.lesson7;
+
+import java.lang.annotation.*;
+
+/**
+ * ru.silantyevmn.java_core_professinal.lesson7
+ * Created by Михаил Силантьев on 07.12.2017.
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test {
+    int priority() default 5;
+}
