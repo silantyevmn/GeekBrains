b130f3dc1cd85968f7c4d44d0048c38e8f701f9c
silantyevmn <5992033@mail.ru>
2017-12-23 22:41:13 +0300

    Добавил массив камней, проверку на столкновение с персонажем. Добавил новый шрифт 96. Если здоровье у персонажа = 0, то рисуем надпись конец игры. Перезапуск игры с новой картой по нажатию на экран/мышь.

--- FacultyAndroidDevelopment/4.The development of games under Android/core/src/ru/silantyevmn/MyGdxGame.java
+++ FacultyAndroidDevelopment/4.The development of games under Android/core/src/ru/silantyevmn/MyGdxGame.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
@@ -12,13 +13,24 @@ public class MyGdxGame extends ApplicationAdapter {
     private Map map;
     private Hero hero;
     private BitmapFont font48;
+    private BitmapFont font96;
+    private Trash[] trashes;
+    private boolean gameOver;
+    Texture textureAsteroid;
 
     @Override
     public void create() {
+        gameOver = false;
         batch = new SpriteBatch();
         map = new Map();
         map.generateMap();
-        hero=new Hero(map,200,300);
+        hero = new Hero(map, 200, 300);
+        textureAsteroid = new Texture("asteroid64.png");
+        trashes = new Trash[50];
+        for (int i = 0; i < trashes.length; i++) {
+            trashes[i] = new Trash(textureAsteroid);
+            trashes[i].prepare();
+        }
         generateFonts();
     }
 
@@ -31,8 +43,41 @@ public class MyGdxGame extends ApplicationAdapter {
         batch.begin();
         map.render(batch);
         hero.render(batch);
+        for (int i = 0; i < trashes.length; i++) {
+            trashes[i].render(batch);
+        }
         hero.renderGUI(batch, font48);
+        if (gameOver) {
+            font96.draw(batch, "Game Over", 330, 400);
+        }
         batch.end();
+    }
+
+    public void update(float dt) {
+        if (hero.getHp() == 0) gameOver = true;
+        if (!gameOver) {
+            map.update(dt);
+            hero.update(dt);
+            for (int i = 0; i < trashes.length; i++) {
+                trashes[i].update(dt);
+                if (hero.getHitArea().overlaps(trashes[i].getHitArea())) {
+                    trashes[i].prepare();
+                    hero.takeDamage(5);
+                }
+            }
+        } else if(Gdx.input.justTouched()) restart();
+    }
+
+    private void restart() {
+        gameOver=false;
+        map = new Map();
+        map.generateMap();
+        hero = new Hero(map, 200, 300);
+        trashes = new Trash[50];
+        for (int i = 0; i < trashes.length; i++) {
+            trashes[i] = new Trash(textureAsteroid);
+            trashes[i].prepare();
+        }
     }
 
     public void generateFonts() {
@@ -46,16 +91,13 @@ public class MyGdxGame extends ApplicationAdapter {
         parameters.shadowOffsetY = 3;
         parameters.shadowColor = Color.BLACK;
         font48 = generator.generateFont(parameters);
+        parameters.size = 96;
+        font96 = generator.generateFont(parameters);
         generator.dispose();
-    }
-    public void update(float dt) {
-        map.update(dt);
-        hero.update(dt);
     }
 
     @Override
     public void dispose() {
-        map.dispoce();
         batch.dispose();
     }
 }
