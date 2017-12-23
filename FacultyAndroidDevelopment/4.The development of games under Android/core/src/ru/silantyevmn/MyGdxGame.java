ea2b24cab7df920e0cdeab5e20f030d4ec2ca738
silantyevmn <5992033@mail.ru>
2017-12-23 21:01:41 +0300

    добавил новые шрифты

--- FacultyAndroidDevelopment/4.The development of games under Android/core/src/ru/silantyevmn/MyGdxGame.java
+++ FacultyAndroidDevelopment/4.The development of games under Android/core/src/ru/silantyevmn/MyGdxGame.java
@@ -2,13 +2,16 @@ package ru.silantyevmn;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 public class MyGdxGame extends ApplicationAdapter {
     private SpriteBatch batch;
     private Map map;
     private Hero hero;
+    private BitmapFont font48;
 
     @Override
     public void create() {
@@ -16,9 +19,8 @@ public class MyGdxGame extends ApplicationAdapter {
         map = new Map();
         map.generateMap();
         hero=new Hero(map,200,300);
+        generateFonts();
     }
-
-
 
     @Override
     public void render() {
@@ -29,9 +31,23 @@ public class MyGdxGame extends ApplicationAdapter {
         batch.begin();
         map.render(batch);
         hero.render(batch);
+        hero.renderGUI(batch, font48);
         batch.end();
     }
 
+    public void generateFonts() {
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("zorque.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameters = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameters.size = 48;
+        parameters.color = Color.WHITE;
+        parameters.borderWidth = 1;
+        parameters.borderColor = Color.BLACK;
+        parameters.shadowOffsetX = 3;
+        parameters.shadowOffsetY = 3;
+        parameters.shadowColor = Color.BLACK;
+        font48 = generator.generateFont(parameters);
+        generator.dispose();
+    }
     public void update(float dt) {
         map.update(dt);
         hero.update(dt);
