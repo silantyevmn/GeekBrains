27ce1c4b7855e3baefe9fea3e857999b43fc96f7
silantyevmn <5992033@mail.ru>
2017-12-29 00:17:44 +0300

    автоформатирование

--- FacultyAndroidDevelopment/4.The development of games under Android/core/src/ru/silantyevmn/PowerUp.java
+++ FacultyAndroidDevelopment/4.The development of games under Android/core/src/ru/silantyevmn/PowerUp.java
@@ -8,16 +8,16 @@ import com.badlogic.gdx.math.Vector2;
  * Created by Михаил Силантьев on 28.12.2017.
  */
 public class PowerUp {
-    public enum Type{
-        MONEY_10(0),MONEY_25(1),MONEY_50(2);
+    public enum Type {
+        MONEY_10(0), MONEY_25(1), MONEY_50(2);
         private int imagePosition;
 
         public int getImagePosition() {
             return imagePosition;
         }
 
-        Type(int imagePosition){
-            this.imagePosition=imagePosition;
+        Type(int imagePosition) {
+            this.imagePosition = imagePosition;
         }
     }
 
@@ -27,11 +27,12 @@ public class PowerUp {
     private Type type;
     private float time;
     private float maxTime;
-    public PowerUp(){
-        this.position=new Vector2(0,0);
-        this.activity=false;
-        this.time=0.0f;
-        this.maxTime=3.0f;
+
+    public PowerUp() {
+        this.position = new Vector2(0, 0);
+        this.activity = false;
+        this.time = 0.0f;
+        this.maxTime = 3.0f;
     }
 
     public Type getType() {
@@ -45,27 +46,36 @@ public class PowerUp {
     public boolean isActivity() {
         return activity;
     }
-    public void activate(float x,float y){
-        activity=true;
-        time=0.0f;
-        position.set(x,y);
-        startY=y;
-        type=Type.values()[MathUtils.random(0,Type.values().length-1)];
-    }
-    public void deactivate(){
-        activity=false;
+
+    public void activate(float x, float y) {
+        activity = true;
+        time = 0.0f;
+        position.set(x, y);
+        startY = y;
+        type = Type.values()[MathUtils.random(0, Type.values().length - 1)];
     }
 
-    public void update(float dt){
-        time+=dt;
-        position.y=startY+15*(float)Math.sin(time*3.0f);
-        if(time>maxTime) deactivate();
+    public void deactivate() {
+        activity = false;
     }
-    public void use(Hero hero){
-        switch (type){
-            case MONEY_10: hero.addMoney(10); break;
-            case MONEY_25: hero.addMoney(25); break;
-            case MONEY_50: hero.addMoney(50); break;
+
+    public void update(float dt) {
+        time += dt;
+        position.y = startY + 15 * (float) Math.sin(time * 3.0f);
+        if (time > maxTime) deactivate();
+    }
+
+    public void use(Hero hero) {
+        switch (type) {
+            case MONEY_10:
+                hero.addMoney(10);
+                break;
+            case MONEY_25:
+                hero.addMoney(25);
+                break;
+            case MONEY_50:
+                hero.addMoney(50);
+                break;
         }
     }
 }
