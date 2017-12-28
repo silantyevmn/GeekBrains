cabb87e5744c4fba2999e410b1de25fc208a1455
silantyevmn <5992033@mail.ru>
2017-12-29 01:56:17 +0300

    добавил atlas картинок

--- FacultyAndroidDevelopment/4.The development of games under Android/core/src/ru/silantyevmn/Trash.java
+++ FacultyAndroidDevelopment/4.The development of games under Android/core/src/ru/silantyevmn/Trash.java
@@ -2,6 +2,7 @@ package ru.silantyevmn;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
@@ -12,38 +13,43 @@ import com.badlogic.gdx.math.Vector2;
  */
 public class Trash {
     private final int RADIUS = 28;
-    private Texture texture;
+    private TextureRegion texture;
     private Vector2 position;
     private Vector2 velocity;
     private Circle hitArea;
     private float scale;
     private float angle;
+    private float speed;
+
 
     public Circle getHitArea() {
         return hitArea;
     }
 
-    public Trash(Texture texture) {
+    public Trash(TextureRegion texture) {
         this.texture = texture;
         this.position = new Vector2(0, 0);
         this.velocity = new Vector2(0, 0);
         this.hitArea = new Circle(position, RADIUS);
+        this.speed=-300;
     }
 
     public void prepare() {
         position.set(MathUtils.random(0, 1280), MathUtils.random(1500, 5000));
-        velocity.set(0, -300.0f);
         hitArea.setPosition(position);
         scale = MathUtils.random(0.6f, 0.8f);
+        speed=scale/0.6f*-300;
+        velocity.set(0, speed);
         hitArea.radius = RADIUS * scale;
         angle = MathUtils.random(0, 360);
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, position.x - 32, position.y - 32, 32, 32, 64, 64, scale, scale, angle, 0, 0, 64, 64, false, false);
+        batch.draw(texture, position.x - 32, position.y - 32, 32, 32, 64, 64, scale, scale, angle);
     }
 
     public void update(float dt) {
+        angle+=dt*200*scale;
         position.mulAdd(velocity, dt);
         if (position.y < -100) {
             prepare();
