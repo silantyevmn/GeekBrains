424877c3347e0dd0900af1dcefd77dd6a36e7974
silantyevmn <5992033@mail.ru>
2017-12-29 00:06:19 +0300

    изменил метод рандом, иеперь монетки разные показываются

--- FacultyAndroidDevelopment/4.The development of games under Android/core/src/ru/silantyevmn/PowerUp.java
+++ FacultyAndroidDevelopment/4.The development of games under Android/core/src/ru/silantyevmn/PowerUp.java
@@ -1,5 +1,6 @@
 package ru.silantyevmn;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 /**
@@ -9,27 +10,47 @@ import com.badlogic.gdx.math.Vector2;
 public class PowerUp {
     public enum Type{
         MONEY_10(0),MONEY_25(1),MONEY_50(2);
-        int indexPosition;
-        Type(int indexPosition){
-            this.indexPosition=indexPosition;
+        private int imagePosition;
+
+        public int getImagePosition() {
+            return imagePosition;
+        }
+
+        Type(int imagePosition){
+            this.imagePosition=imagePosition;
         }
     }
+
     private Vector2 position;
+    private float startY;
     private boolean activity;
     private Type type;
     private float time;
     private float maxTime;
-
     public PowerUp(){
         this.position=new Vector2(0,0);
         this.activity=false;
         this.time=0.0f;
-        this.maxTime=5.0f;
+        this.maxTime=3.0f;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public boolean isActivity() {
+        return activity;
     }
     public void activate(float x,float y){
         activity=true;
+        time=0.0f;
         position.set(x,y);
-        type=Type.values()[(int)Math.random()*Type.values().length];
+        startY=y;
+        type=Type.values()[MathUtils.random(0,Type.values().length-1)];
     }
     public void deactivate(){
         activity=false;
@@ -37,6 +58,14 @@ public class PowerUp {
 
     public void update(float dt){
         time+=dt;
+        position.y=startY+15*(float)Math.sin(time*3.0f);
         if(time>maxTime) deactivate();
+    }
+    public void use(Hero hero){
+        switch (type){
+            case MONEY_10: hero.addMoney(10); break;
+            case MONEY_25: hero.addMoney(25); break;
+            case MONEY_50: hero.addMoney(50); break;
+        }
     }
 }
